aa04d21fb081664aaaec3a57ec71a0c05d924f93
Tomas Zaicevas <tozaicevas@gmail.com>
2018-12-12 14:41:08 +0200

    linting fixes

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { withNavigationFocus } from 'react-navigation';
+import PropTypes from 'prop-types';
 import CustomCamera from '../components/Camera/CustomCamera';
 
 class CameraScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+    static navigationOptions = {
+        header: null,
+    };
 
-  render() {
-    return <CustomCamera isScreenFocused={this.props.isFocused} />;
-  }
+    render() {
+        return <CustomCamera isScreenFocused={this.props.isFocused} />;
+    }
 }
+
+CameraScreen.propTypes = {
+    isFocused: PropTypes.bool.isRequired,
+};
 
 export default withNavigationFocus(CameraScreen);
