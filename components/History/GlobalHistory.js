4b93dcd37464552f92e1d08b8e30851d6529b926
Tomas Zaicevas <tozaicevas@gmail.com>
2018-12-14 13:22:59 +0200

    init

--- components/History/GlobalHistory.js
+++ components/History/GlobalHistory.js
@@ -1,144 +1,174 @@
-import React from 'react';
+import React from "react";
 import {
-    Container,
-    Content,
-    List,
-    ListItem,
-    Left,
-    Body,
-    Right,
-    Thumbnail,
-    Text,
-} from 'native-base';
-import { View, ActivityIndicator } from 'react-native';
+  Container,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Text
+} from "native-base";
+import { View, ActivityIndicator, RefreshControl } from "react-native";
 
 const getDate = timestamp => timestamp.substr(0, 10);
 const getHours = timestamp => timestamp.substr(11, 19);
 
-const SingleTimestamp = (props) => {
-    const { timestamp } = props;
-    const searchReason = ['Not searched', 'Missing', 'Criminal', 'Other'];
-    return (
-        <ListItem avatar>
-            <Left>
-                <Thumbnail
-                    small
-                    source={{ uri: `data:image/png;base64, ${timestamp.missingModel.baseImage}` }}
-                />
-            </Left>
-            <Body>
-                <Text>
-                    {`${timestamp.missingModel.firstName} ${timestamp.missingModel.lastName}`}
-                </Text>
-                <Text note>{searchReason[timestamp.missingModel.reason]}</Text>
-            </Body>
-            <Right>
-                <Text note>
-                    {`${getDate(timestamp.dateAndTime)}\u000A${getHours(timestamp.dateAndTime)}`}
-                </Text>
-            </Right>
-        </ListItem>
-    );
+const SingleTimestamp = props => {
+  const { timestamp } = props;
+  const searchReason = ["Not searched", "Missing", "Criminal", "Other"];
+  return (
+    <ListItem avatar>
+      <Left>
+        <Thumbnail
+          small
+          source={{
+            uri: `data:image/png;base64, ${timestamp.missingModel.baseImage}`
+          }}
+        />
+      </Left>
+      <Body>
+        <Text>
+          {`${timestamp.missingModel.firstName} ${timestamp.missingModel
+            .lastName}`}
+        </Text>
+        <Text note>{searchReason[timestamp.missingModel.reason]}</Text>
+      </Body>
+      <Right>
+        <Text note>
+          {`${getDate(timestamp.dateAndTime)}\u000A${getHours(
+            timestamp.dateAndTime
+          )}`}
+        </Text>
+      </Right>
+    </ListItem>
+  );
 };
 
-const AllTimestamps = (props) => {
-    const { timestampList } = props;
-    return timestampList.map(timestamp => (
-        <SingleTimestamp key={timestamp.id} timestamp={timestamp} />
-    ));
+const AllTimestamps = props => {
+  const { timestampList } = props;
+  return timestampList.map(timestamp => (
+    <SingleTimestamp key={timestamp.id} timestamp={timestamp} />
+  ));
 };
 
 class GlobalHistory extends React.Component {
-    state = {
-        timestampList: [],
-        isFetchingData: true,
-    };
+  state = {
+    timestampList: [],
+    isFetchingData: true,
+    refreshing: false
+  };
 
-    componentDidMount() {
-        this.setState({ isFetchingData: true });
-        fetch('https://epicentereu.azurewebsites.net/api/timestamps', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => {
-                this.setState({ isFetchingData: false });
-                if (response.status !== 200) return;
-                response.json().then((rjson) => {
-                    console.log(':-))))))))))))');
-                    this.setState({ timestampList: rjson });
-                });
-            })
-            .catch((x) => {
-                this.setState({ isFetchingData: false });
-                console.log(`${x}in globalhistory`);
-            });
-    }
+  componentDidMount() {
+    this.setState({ isFetchingData: true });
+    fetch("https://epicentereu.azurewebsites.net/api/timestamps", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        this.setState({ isFetchingData: false });
+        if (response.status !== 200) return;
+        response.json().then(rjson => {
+          console.log(":-))))))))))))");
+          this.setState({ timestampList: rjson });
+        });
+      })
+      .catch(x => {
+        this.setState({ isFetchingData: false });
+        console.log(`${x} in globalhistory`);
+      });
+  }
 
-    componentDidUpdate(prevProps) {
-        if (!prevProps.isScreenFocused && this.props.isScreenFocused) {
-            this.componentDidMount();
-        }
-    }
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    fetch("https://epicentereu.azurewebsites.net/api/timestamps", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        this.setState({ refreshing: false });
+        if (response.status !== 200) return;
+        response.json().then(rjson => {
+          console.log(":-))))))))))))");
+          this.setState({ timestampList: rjson });
+        });
+      })
+      .catch(x => {
+        this.setState({ refreshing: false });
+        console.log(`${x} in globalhistory`);
+      });
+  };
 
-    render() {
-        return this.state.isFetchingData ? (
-            <Container>
-                <ActivityIndicator
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-around',
-                        padding: 20,
-                        zIndex: 20,
-                    }}
-                    size="large"
-                    color="blue"
-                />
-                <Content>
-                    <List>
-                        <AllTimestamps timestampList={this.state.timestampList} />
-                    </List>
-                </Content>
-            </Container>
-        ) : (
-            <Container>
-                <Content>
-                    <List>
-                        <AllTimestamps timestampList={this.state.timestampList} />
-                    </List>
-                </Content>
-            </Container>
-        );
+  componentDidUpdate(prevProps) {
+    if (!prevProps.isScreenFocused && this.props.isScreenFocused) {
+      this.componentDidMount();
     }
+  }
+
+  render() {
+    return this.state.isFetchingData ? (
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            padding: 20
+          }}
+          size="large"
+          color="blue"
+        />
+        <NewReceiptIcon navigation={this.props.navigation} />
+      </View>
+    ) : (
+      <Container>
+        <Content
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh}
+            />
+          }
+        >
+          <List>
+            <AllTimestamps timestampList={this.state.timestampList} />
+          </List>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 const styles = {
-    rootContainer: {
-        position: 'relative',
-        height: '100%',
-        width: '100%',
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    horizontal: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        padding: 10,
-    },
-    helpContainer: {
-        marginTop: 5,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        fontSize: 14,
-        color: '#2e78b7',
-    },
+  rootContainer: {
+    position: "relative",
+    height: "100%",
+    width: "100%"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  },
+  helpContainer: {
+    marginTop: 5,
+    alignItems: "center"
+  },
+  helpLink: {
+    paddingVertical: 15
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: "#2e78b7"
+  }
 };
 
 export default GlobalHistory;
